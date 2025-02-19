76060e4129503c39146ea0162fb138869a2fa72e
Alexander Makarov <i@kremovtort.ru>
2025-02-19 14:00:05 +0300

    feat: add aider

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,27 +14,9 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "24.11"; # Please read the comment before changing.
-  
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    pkgs.aider-chat
     pkgs.docker
     pkgs.lazydocker
     pkgs.lazygit
@@ -42,7 +24,6 @@
     pkgs.nil
     pkgs.ripgrep
     pkgs.pipx
-    pkgs.starship
     pkgs.uv
     pkgs.wezterm
     
@@ -55,6 +36,9 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".aider.conf.yml".source = ~/dotfiles/aider/.aider.conf.yml;
+    ".aider.metadata.json".source = ~/dotfiles/aider/.aider.metadata.json;
+    ".aider.model.settings.yml".source = ~/dotfiles/aider/.aider.model.settings.yml;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
