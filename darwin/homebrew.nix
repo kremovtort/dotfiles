0bd270d39335faff54d6332fe4e5b473426b2a4c
Alexander Makarov <i@kremovtort.ru>
2025-07-28 19:04:54 +0300

    fix(darwin): remove casks from darwin/homebrew.nix

--- darwin/homebrew.nix
+++ darwin/homebrew.nix
@@ -3,7 +3,6 @@
   global.autoUpdate = false;
   onActivation = {
     autoUpdate = false;
-    cleanup = "zap";
     extraFlags = [ "--verbose" ];
     upgrade = false;
   };
@@ -14,21 +13,6 @@
       start_service = true;
       restart_service = "changed";
     }
-  ];
-  casks = [
-    "cursor"
-    "dockdoor"
-    "ghostty"
-    "karabiner-elements"
-    "monokle"
-    "obsidian"
-    "ollama"
-    "podman-desktop"
-    "scroll-reverser"
-    "telegram"
-    "visual-studio-code"
-    "yandex-music"
-    "zen-browser"
   ];
   taps = [
     "homebrew/services"
