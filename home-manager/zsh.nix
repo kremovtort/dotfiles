c7cf543154f6e3eb502fcb3e612d71ed012def8e
Alexander Makarov <i@kremovtort.ru>
2025-08-19 23:11:01 +0300

    add vi mode to zsh

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -14,17 +14,15 @@
     { name = "zsh-completions"; src = pkgs.zsh-completions.src; }
     { name = "fast-syntax-highlighting"; src = pkgs.zsh-fast-syntax-highlighting.src; }
     { name = "fzf-tab"; src = pkgs.zsh-fzf-tab.src; }
+    { name = "zsh-vi-mode"; src = pkgs.zsh-vi-mode.src; }
   ];
   initContent = ''
+    CATPUCCIN_COLORS_FZF="bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8,selected-bg:#45475a"
+
     export FZF_DEFAULT_OPTS=" \
-      --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-      --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-      --color=selected-bg:#45475a \
-      --multi --prompt='❯ ' --marker='+'"
+      --color=$CATPUCCIN_COLORS_FZF \
+      --multi --prompt='❯ ' --marker='+' --border=none"
       
-    export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --layout=reverse --preview='lla-for-fzf {}'"
-    
-    bindkey '^k' 'cd "$(zoxide query -a -i)"'
+    export _ZO_FZF_OPTS="--color=$CATPUCCIN_COLORS_FZF --reverse --preview='lla-for-fzf {}' --height=~50% --prompt='❯ ' --preview-border=line"
   '';
 }
