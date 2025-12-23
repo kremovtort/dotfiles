ae2fa03c5fe7acb1730c7ae4ea7a2da319bdb003
Alexander Makarov <i@kremovtort.ru>
2025-12-23 21:45:20 +0300

    refactor neovim keybindings for vscode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,20 +3,27 @@
 -- Add any additional keymaps here
 if vim.g.vscode then
   -- Remap folding keys
-  vim.keymap.set("n", "zM", '<Cmd>call VSCodeNotify("editor.foldAll")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "zR", '<Cmd>call VSCodeNotify("editor.unfoldAll")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "zc", '<Cmd>call VSCodeNotify("editor.fold")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "zC", '<Cmd>call VSCodeNotify("editor.foldRecursively")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "zo", '<Cmd>call VSCodeNotify("editor.unfold")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "zO", '<Cmd>call VSCodeNotify("editor.unfoldRecursively")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "za", '<Cmd>call VSCodeNotify("editor.toggleFold")<CR>', { noremap = true, silent = true })
+  local function vscode(command)
+    return '<Cmd>call VSCodeNotify("' .. command .. '")<CR>'
+  end
+  
+  local noremap_silent = { noremap = true, silent = true }
+
+  vim.keymap.set("n", "zM", vscode("editor.foldAll"), noremap_silent)
+  vim.keymap.set("n", "zR", vscode("editor.unfoldAll"), noremap_silent)
+  vim.keymap.set("n", "zc", vscode("editor.fold"), noremap_silent)
+  vim.keymap.set("n", "zC", vscode("editor.foldRecursively"), noremap_silent)
+  vim.keymap.set("n", "zo", vscode("editor.unfold"), noremap_silent)
+  vim.keymap.set("n", "zO", vscode("editor.unfoldRecursively"), noremap_silent)
+  vim.keymap.set("n", "za", vscode("editor.toggleFold"), noremap_silent)
   
   -- Remap error navigation keys
-  vim.keymap.set("n", "]e", '<Cmd>call VSCodeNotify("editor.action.marker.next")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "[e", '<Cmd>call VSCodeNotify("editor.action.marker.prev")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "]E", '<Cmd>call VSCodeNotify("editor.action.marker.nextInFiles")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "[E", '<Cmd>call VSCodeNotify("editor.action.marker.prevInFiles")<CR>', { noremap = true, silent = true })
-  vim.keymap.set("n", "<C-w>q", '<Cmd>call VSCodeNotify("workbench.action.closeEditorsInGroup")<CR>', { noremap = true, silent = true })
+  vim.keymap.set("n", "]e", vscode("editor.action.marker.next"), noremap_silent)
+  vim.keymap.set("n", "[e", vscode("editor.action.marker.prev"), noremap_silent)
+  vim.keymap.set("n", "]E", vscode("editor.action.marker.nextInFiles"), noremap_silent)
+  vim.keymap.set("n", "[E", vscode("editor.action.marker.prevInFiles"), noremap_silent)
+  vim.keymap.set("n", "<C-w>q", vscode("workbench.action.closeEditorsInGroup"), noremap_silent)
+  vim.keymap.set("n", "<leader>e", vscode("workbench.files.action.focusFilesExplorer"), noremap_silent)
 end
 
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>")
