d2e215eef35fa9634c6b450654d6112bb41d1fc5
Alexander Makarov <i@kremovtort.ru>
2025-12-24 00:43:35 +0300

    add more vscode-neovim keybindings

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 if vim.g.vscode then
-  -- Remap folding keys
   local function vscode(command)
     return '<Cmd>call VSCodeNotify("' .. command .. '")<CR>'
   end
   
   local noremap_silent = { noremap = true, silent = true }
 
+  -- Remap folding keys
   vim.keymap.set("n", "zM", vscode("editor.foldAll"), noremap_silent)
   vim.keymap.set("n", "zR", vscode("editor.unfoldAll"), noremap_silent)
   vim.keymap.set("n", "zc", vscode("editor.fold"), noremap_silent)
@@ -24,6 +24,15 @@ if vim.g.vscode then
   vim.keymap.set("n", "[E", vscode("editor.action.marker.prevInFiles"), noremap_silent)
   vim.keymap.set("n", "<C-w>q", vscode("workbench.action.closeEditorsInGroup"), noremap_silent)
   vim.keymap.set("n", "<leader>e", vscode("workbench.files.action.focusFilesExplorer"), noremap_silent)
+
+  -- Remap navigation keys
+  vim.keymap.set("n", "gr", vscode("editor.action.goToReferences"), noremap_silent)
+  vim.keymap.set("n", "gi", vscode("editor.action.goToImplementation"), noremap_silent)
+  vim.keymap.set("n", "gt", vscode("editor.action.goToTypeDefinition"), noremap_silent)
+  
+  vim.keymap.set("n", "<leader>m", vscode("haskell-modules.search"), noremap_silent)
+  vim.keymap.set("n", "<leader>s", vscode("workbench.action.gotoSymbol"), noremap_silent)
+  vim.keymap.set("n", "<leader>S", vscode("workbench.action.showAllSymbols"), noremap_silent)
 end
 
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>")
--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -4,7 +4,9 @@ return {
     enabled = true,
     keys = {
       { "s", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
+      { "ы", "<Plug>(leap-forward)", mode = { "n", "x", "o" }, desc = "Leap Forward to" },
       { "S", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
+      { "Ы", "<Plug>(leap-backward)", mode = { "n", "x", "o" }, desc = "Leap Backward to" },
       { "gs", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from Windows" },
     },
     config = function(_, opts)
