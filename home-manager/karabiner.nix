928e4b07f6e8b85f42d2feabb71bfb8127b7aaba
Alexander Makarov <i@kremovtort.ru>
2026-01-17 16:08:48 +0300

    karabiner: update fn layer rules

--- home-manager/karabiner.nix
+++ home-manager/karabiner.nix
@@ -3,102 +3,239 @@ let
   karabinix = inputs.karabinix.lib;
   # Import rules module directly since not all functions are exported in lib
   rules = import "${inputs.karabinix}/lib/rules.nix" { inherit lib; };
-  inherit (karabinix) 
-    keyCodes 
+  inherit (karabinix)
+    keyCodes
     mkProfile
-    mkToEvent;
+    mkToEvent
+    ;
   inherit (rules)
-    mkManipulator 
-    mkFromEvent 
-    mkRule 
+    mkManipulator
+    mkFromEvent
+    mkRule
     mkModifiers
-    mkCondition;
+    mkCondition
+    ;
 
   mkLayerManipulators = modifier: [
     # Navigation
-    { from = "h"; to = "left_arrow"; }
-    { from = "j"; to = "down_arrow"; }
-    { from = "k"; to = "up_arrow"; }
-    { from = "l"; to = "right_arrow"; }
+    {
+      from = "h";
+      to = "left_arrow";
+    }
+    {
+      from = "j";
+      to = "down_arrow";
+    }
+    {
+      from = "k";
+      to = "up_arrow";
+    }
+    {
+      from = "l";
+      to = "right_arrow";
+    }
 
     # Tab
-    { from = "n"; to = "tab"; }
-    { from = "p"; to = "tab"; toModifiers = [ "left_shift" ]; }
-    
+    {
+      from = "n";
+      to = "tab";
+    }
+    {
+      from = "p";
+      to = "tab";
+      toModifiers = [ "left_shift" ];
+    }
+
     # Media
-    { from = "spacebar"; to = "play_or_pause"; }
-    { from = "z"; to = "rewind"; }
-    { from = "x"; to = "fastforward"; }
+    {
+      from = "spacebar";
+      to = "play_or_pause";
+    }
+    {
+      from = "z";
+      to = "rewind";
+    }
+    {
+      from = "x";
+      to = "fastforward";
+    }
 
     # Volume
-    { from = "comma"; to = "volume_down"; }
-    { from = "period"; to = "volume_up"; }
-    
+    {
+      from = "comma";
+      to = "volume_down";
+    }
+    {
+      from = "period";
+      to = "volume_up";
+    }
+
     # F-Keys
-    { from = "1"; to = "f1"; toModifiers = [ "fn" ]; }
-    { from = "2"; to = "f2"; toModifiers = [ "fn" ]; }
-    { from = "3"; to = "f3"; toModifiers = [ "fn" ]; }
-    { from = "4"; to = "f4"; toModifiers = [ "fn" ]; }
-    { from = "5"; to = "f5"; toModifiers = [ "fn" ]; }
-    { from = "6"; to = "f6"; toModifiers = [ "fn" ]; }
-    { from = "7"; to = "f7"; toModifiers = [ "fn" ]; }
-    { from = "8"; to = "f8"; toModifiers = [ "fn" ]; }
-    { from = "9"; to = "f9"; toModifiers = [ "fn" ]; }
-    { from = "0"; to = "f10"; toModifiers = [ "fn" ]; }
-    { from = "hyphen"; to = "f11"; toModifiers = [ "fn" ]; }
-    { from = "equal_sign"; to = "f12"; toModifiers = [ "fn" ]; }
-    
+    {
+      from = "1";
+      to = "f1";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "2";
+      to = "f2";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "3";
+      to = "f3";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "4";
+      to = "f4";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "5";
+      to = "f5";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "6";
+      to = "f6";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "7";
+      to = "f7";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "8";
+      to = "f8";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "9";
+      to = "f9";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "0";
+      to = "f10";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "hyphen";
+      to = "f11";
+      toModifiers = [ "fn" ];
+    }
+    {
+      from = "equal_sign";
+      to = "f12";
+      toModifiers = [ "fn" ];
+    }
+
     # System
-    { from = "open_bracket"; to = "escape"; }
-    { from = "o"; to = "return_or_enter"; }
-    { from = "delete_or_backspace"; to = "delete_forward"; }
-    { from = "d"; to = "page_down"; }
-    { from = "u"; to = "page_up"; }
-    { from = "a"; to = "home"; }
-    { from = "e"; to = "end"; }
-    { from = "w"; to = "delete_or_backspace"; toModifiers = [ "left_option" ]; }
+    {
+      from = "open_bracket";
+      to = "escape";
+    }
+    {
+      from = "o";
+      to = "return_or_enter";
+    }
+    {
+      from = "delete_or_backspace";
+      to = "delete_forward";
+    }
+    {
+      from = "d";
+      to = "page_down";
+    }
+    {
+      from = "u";
+      to = "page_up";
+    }
+    {
+      from = "a";
+      to = "home";
+    }
+    {
+      from = "e";
+      to = "end";
+    }
+    {
+      from = "w";
+      to = "delete_or_backspace";
+      toModifiers = [ "left_option" ];
+    }
   ];
 
   # Преобразование конфига в манипуляторы karabinix
-  createManipulators = modifier: map (m: mkManipulator {
-    from = mkFromEvent {
-      key_code = keyCodes.${m.from};
-      modifiers = mkModifiers { mandatory = [ modifier ]; optional = [ "any" ]; };
-    };
-    to = [
-      (mkToEvent ({
-        key_code = keyCodes.${m.to};
-      } // lib.optionalAttrs (m ? toModifiers) { modifiers = m.toModifiers; }))
-    ];
-  }) (mkLayerManipulators modifier);
+  createManipulators =
+    modifier:
+    map (
+      m:
+      mkManipulator {
+        from = mkFromEvent {
+          key_code = keyCodes.${m.from};
+          modifiers = mkModifiers {
+            mandatory = [ modifier ];
+            optional = [ "any" ];
+          };
+        };
+        to = [
+          (mkToEvent (
+            {
+              key_code = keyCodes.${m.to};
+            }
+            // lib.optionalAttrs (m ? toModifiers) { modifiers = m.toModifiers; }
+          ))
+        ];
+      }
+    ) (mkLayerManipulators modifier);
 
   # Условие для конкретного устройства
-  deviceCondition = deviceId: mkCondition {
-    type = "device_if";
-    identifiers = [ deviceId ];
-  };
+  deviceCondition =
+    deviceId:
+    mkCondition {
+      type = "device_if";
+      identifiers = [ deviceId ];
+    };
 
   # Правила для специфичных устройств
   mkDeviceSpecificRules = description: deviceId: [
     (mkManipulator {
       conditions = [ (deviceCondition deviceId) ];
       from = mkFromEvent { key_code = "f23"; };
-      to = [ (mkToEvent { key_code = keyCodes.left_arrow; modifiers = [ "left_control" ]; }) ];
+      to = [
+        (mkToEvent {
+          key_code = keyCodes.left_arrow;
+          modifiers = [ "left_control" ];
+        })
+      ];
     })
     (mkManipulator {
       conditions = [ (deviceCondition deviceId) ];
       from = mkFromEvent { key_code = "f24"; };
-      to = [ (mkToEvent { key_code = keyCodes.right_arrow; modifiers = [ "left_control" ]; }) ];
+      to = [
+        (mkToEvent {
+          key_code = keyCodes.right_arrow;
+          modifiers = [ "left_control" ];
+        })
+      ];
     })
   ];
 
   # Стандартные F1-F12 для Keychron
   keychronFnKeys = map (i: {
-    from = { key_code = "f${toString i}"; };
-    to = [{ key_code = "f${toString i}"; }];
+    from = {
+      key_code = "f${toString i}";
+    };
+    to = [ { key_code = "f${toString i}"; } ];
   }) (lib.range 1 12);
 
-in {
+in
+{
+  imports = [ inputs.karabinix.homeManagerModules.karabinix ];
+
   services.karabinix = {
     enable = true;
     configuration = {
@@ -111,7 +248,9 @@ in {
         (mkProfile {
           name = "Default";
           selected = true;
-          virtual_hid_keyboard = { keyboard_type_v2 = "ansi"; };
+          virtual_hid_keyboard = {
+            keyboard_type_v2 = "ansi";
+          };
           complex_modifications = {
             parameters = {
               "basic.simultaneous_threshold_milliseconds" = 50;
@@ -120,31 +259,50 @@ in {
               "basic.to_if_held_down_threshold_milliseconds" = 500;
             };
             rules = [
-              (mkRule "F19 to switch language" [
+              (mkRule "Fn tap to Option+Space" [
                 (mkManipulator {
-                  from = mkFromEvent { key_code = keyCodes.f19; };
+                  from = mkFromEvent { key_code = "fn"; };
                   to = [
-                    (mkToEvent { key_code = keyCodes.spacebar; modifiers = [ "left_option" ]; })
+                    (mkToEvent { key_code = "fn"; })
+                  ];
+                  to_if_alone = [
+                    (mkToEvent {
+                      key_code = "spacebar";
+                      modifiers = [ "left_option" ];
+                    })
                   ];
                 })
               ])
-              (mkRule "Layer (Ctrl/Fn)" (
-                (createManipulators "left_control") ++ 
-                (createManipulators "fn")
+              (mkRule "Layer (Ctrl/Fn)" ((createManipulators "left_control") ++ (createManipulators "fn")))
+              (mkRule "BSK V3 PRO BT Specific" (
+                mkDeviceSpecificRules "BSK V3 PRO BT" {
+                  product_id = 172;
+                  vendor_id = 1678;
+                }
               ))
-              (mkRule "BSK V3 PRO BT Specific" (mkDeviceSpecificRules "BSK V3 PRO BT" { product_id = 172; vendor_id = 1678; }))
-              (mkRule "BSK V3 PRO G Specific" (mkDeviceSpecificRules "BSK V3 PRO G" { product_id = 171; vendor_id = 5426; }))
+              (mkRule "BSK V3 PRO G Specific" (
+                mkDeviceSpecificRules "BSK V3 PRO G" {
+                  product_id = 171;
+                  vendor_id = 5426;
+                }
+              ))
             ];
           };
           devices = [
             {
-              identifiers = { is_keyboard = true; product_id = 171; vendor_id = 5426; };
+              identifiers = {
+                is_keyboard = true;
+                product_id = 171;
+                vendor_id = 5426;
+              };
               simple_modifications = [
-                { 
-                  from = { key_code = "f22"; }; 
-                  to = [ 
-                    { apple_vendor_keyboard_key_code = "mission_control"; } 
-                  ]; 
+                {
+                  from = {
+                    key_code = "f22";
+                  };
+                  to = [
+                    { apple_vendor_keyboard_key_code = "mission_control"; }
+                  ];
                 }
               ];
             }
@@ -184,8 +342,10 @@ in {
             }
           ];
           simple_modifications = [
-            { 
-              from = { key_code = "f22"; };
+            {
+              from = {
+                key_code = "f22";
+              };
               to = [
                 { apple_vendor_keyboard_key_code = "mission_control"; }
               ];
@@ -196,4 +356,3 @@ in {
     };
   };
 }
-
