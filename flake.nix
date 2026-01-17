63cdb8684cc4271053838fa32a234f4634c7244c
Alexander Makarov <i@kremovtort.ru>
2026-01-17 16:08:48 +0300

    flake: add openspec and nvim overlays

--- flake.nix
+++ flake.nix
@@ -3,82 +3,102 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    zjstatus.url = "github:dj95/zjstatus";
+    karabinix.url = "github:pepegar/karabinix";
+
     nix-darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     paneru = {
       url = "github:karinushka/paneru";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    zjstatus.url = "github:dj95/zjstatus";
-    karabinix.url = "github:pepegar/karabinix";
+
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nvim-flake = {
+    nvim = {
       url = "path:./nvim-flake";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixvim.inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    openspec = {
+      url = "github:Fission-AI/OpenSpec";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs @ { flake-parts, self, ... }:
+  outputs =
+    inputs@{ flake-parts, self, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "aarch64-darwin" "aarch64-linux" "x86_64-linux" ];
+      systems = [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-      perSystem = { inputs', pkgs, system, ... }: {
-        _module.args.pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [
-            (final: prev: {
-              zjstatus = inputs.zjstatus.packages.${prev.stdenv.hostPlatform.system}.default;
-            })
-          ];
-          config = { };
-        };
-        packages.just = inputs'.nixpkgs.legacyPackages.just;
-        packages.home-manager = inputs'.home-manager.packages.home-manager;
-        packages.darwin-rebuild = inputs'.nix-darwin.packages.darwin-rebuild;
+      perSystem =
+        {
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              (final: prev: {
+                zjstatus = inputs.zjstatus.packages.${prev.stdenv.hostPlatform.system}.default;
+                openspec = inputs.openspec.packages.${prev.stdenv.hostPlatform.system}.default;
+                nvim4vscode = inputs.nvim.packages.${prev.stdenv.hostPlatform.system}.nvim4vscode;
+              })
+            ];
+            config = { };
+          };
+          packages.just = inputs'.nixpkgs.legacyPackages.just;
+          packages.home-manager = inputs'.home-manager.packages.home-manager;
+          packages.darwin-rebuild = inputs'.nix-darwin.packages.darwin-rebuild;
 
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.bash-language-server
-            pkgs.just
-            pkgs.lua-language-server
-            pkgs.nixd
-            pkgs.nixfmt
-            pkgs.statix
-            pkgs.shellcheck
-            pkgs.stylua
-          ];
-        };
-        
-        legacyPackages.homeConfigurations."kremovtort" = inputs.home-manager.lib.homeManagerConfiguration {
-          inherit pkgs;
-          modules = [
-            inputs.karabinix.homeManagerModules.karabinix
-            inputs.paneru.homeModules.paneru
-            inputs.sops-nix.homeManagerModules.sops
-            inputs.nvim-flake.homeModules.default
-            ./home-manager/home.nix
-          ];
-          extraSpecialArgs = {
-            inherit system inputs;
-            flake-self = self;
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.bash-language-server
+              pkgs.just
+              pkgs.lua-language-server
+              pkgs.nixd
+              pkgs.nixfmt
+              pkgs.statix
+              pkgs.shellcheck
+              pkgs.stylua
+            ];
+          };
+
+          legacyPackages.homeConfigurations."kremovtort" = inputs.home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [
+              inputs.nvim.homeModules.default
+              ./home-manager/home.nix
+            ];
+            extraSpecialArgs = {
+              inherit system inputs;
+              flake-self = self;
+            };
+          };
+
+          legacyPackages.darwinConfigurations."kremovtort-OSX" = inputs.nix-darwin.lib.darwinSystem {
+            modules = [ ./darwin/configuration.nix ];
           };
         };
-        
-        legacyPackages.darwinConfigurations."kremovtort-OSX" = inputs.nix-darwin.lib.darwinSystem {
-          modules = [ ./darwin/configuration.nix ];
-        };
-      };
     };
 }
