9b9349e72ae669402616c5930e98f29a96d8f526
Alexander Makarov <i@kremovtort.ru>
2026-01-21 00:22:17 +0300

    add togglable jjui

--- nvim/keymaps.nix
+++ nvim/keymaps.nix
@@ -287,6 +287,47 @@
       options.desc = "Toggle terminal";
     }
 
+    # jjui in floating toggleterm
+    {
+      mode = [
+        "n"
+        "t"
+      ];
+      key = "<D-j>";
+      action.__raw = ''
+        function()
+          local Terminal = require("toggleterm.terminal").Terminal
+
+          if not _G.__jjui_term then
+            _G.__jjui_term = Terminal:new({
+              cmd = "jjui",
+              direction = "float",
+              hidden = true,
+              close_on_exit = false,
+              on_open = function(_)
+                vim.cmd("startinsert!")
+              end,
+              on_exit = function(_, _, exit_code)
+                -- If jjui exited successfully, hide the terminal.
+                if exit_code == 0 then
+                  pcall(vim.cmd, "close")
+                end
+                -- Always drop the instance so next open is fresh.
+                _G.__jjui_term = nil
+              end,
+              -- Run in current Neovim cwd
+              cwd = function()
+                return (vim.uv or vim.loop).cwd() or vim.fn.getcwd()
+              end,
+            })
+          end
+
+          _G.__jjui_term:toggle()
+        end
+      '';
+      options.desc = "jjui (float)";
+    }
+
     # Escape and clear search highlight
     {
       mode = [
