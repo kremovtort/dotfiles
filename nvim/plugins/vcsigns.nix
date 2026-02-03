84908a102e3181ab7e2199e4ffbacf0e2f04280d
Alexander Makarov <i@kremovtort.ru>
2026-02-03 21:27:43 +0300

    feature(neovim): support arcadia signs

--- nvim/plugins/vcsigns.nix
+++ nvim/plugins/vcsigns.nix
@@ -23,6 +23,30 @@ in
   ];
 
   extraConfigLua = lib.mkAfter ''
+    local common = require("vcsigns.repo_def.common")
+    local util = require("vcsigns.util")
+    require("vcsigns.repo").register_vcs({
+      name = "Arc",
+      detect = {
+        cmd = function()
+          return { "arc", "root" }
+        end,
+        check = common.check_and_extract_root,
+      },
+      show = function(target, root, lines_cb)
+        local cmd = {
+          "arc",
+          "show",
+          string.format("HEAD~%d", target.commit) .. ":" .. target.file,
+        }
+        util.run_with_timeout(cmd, { cwd = root }, function(out)
+          lines_cb(common.content_to_lines(out.stdout))
+        end)
+      end,
+      -- Rename resolution not implemented for git.
+      resolve_rename = nil,
+    })
+
     require("vcsigns").setup({
       target_commit = 1,
     })
