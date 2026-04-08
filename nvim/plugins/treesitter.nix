a3002aede5fc51a20b2c375916d3e063b75d32fa
Alexander Makarov <i@kremovtort.ru>
2026-04-08 11:37:00 +0300

    fix(neovim): don't show treesitter context in nofile buffers

--- nvim/plugins/treesitter.nix
+++ nvim/plugins/treesitter.nix
@@ -8,7 +8,15 @@
     };
   };
 
-  plugins.treesitter-context.enable = lib.mkDefault true;
+  plugins.treesitter-context = {
+    enable = lib.mkDefault true;
+    settings.on_attach.__raw = ''
+      function(buf)
+        return vim.bo[buf].buftype ~= "nofile"
+      end
+    '';
+  };
+ 
   colorschemes.catppuccin.settings.integrations.treesitter_context.enable = lib.mkDefault true;
 
   plugins.ts-context-commentstring.enable = true;
